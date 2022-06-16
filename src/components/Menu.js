e866be8ee2a21f4fa2484e6766c2cc313120430a
Srikanta-K <104119342+Srikanta-K@users.noreply.github.com>
2022-06-16 14:13:06 +0530

    inital commit

---
+++ src/components/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+
+const Menu = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="white">
+                <Container>
+                    <Navbar.Brand style={{ width: "100%", marginTop: "2%", marginLeft: "20%" }}><b>Welcome To User Registration page</b></Navbar.Brand>
+
+                </Container>
+
+                <Container>
+                <Navbar.Brand style={{ width: "100%", marginTop: "2%", marginLeft: "30%" }}><b>CAPGEMINI LOGO</b></Navbar.Brand>
+                </Container>
+
+            </Navbar>
+
+        </div>
+    )
+}
+
+export default Menu
